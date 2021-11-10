fddc1d6025555517ca52de57aac8e8b5073b5a94
Michael Paquette <jotha885@gmail.com>
2021-11-10 11:40:29 +0100

    added density to scatter plot

--- ismrm2022/figure_correlation_g_l.py
+++ ismrm2022/figure_correlation_g_l.py
@@ -74,10 +74,61 @@ pl.close()
 
 
 
+from scipy.stats import gaussian_kde
+
+x = geom_graph.ravel()
+y = len_graph.ravel()
+# Calculate the point density
+xy = np.vstack([x,y])
+z = gaussian_kde(xy)(xy)
+
+# Sort the points by density, so that the densest points are plotted last
+idx = z.argsort()
+x, y, z = x[idx], y[idx], z[idx]
 
 
 
 
 
+
+
+
+pl.figure()
+pl.scatter(x, y, c=z, s=50, 
+	       alpha=0.5,
+	       edgecolors='none')
+
+
+pl.xlabel(r'$$\sqrt[n]{{\prod_{{i\in\text{{path}}}}^{{n = |\text{{path}}|}} p_i}}$$', fontsize=16)
+pl.ylabel(r'path length (\# vertex)', fontsize=16)
+pl.title('Geometric Mean of Path Probability vs Length', fontsize=18)
+
+
+
+R = np.corrcoef(geom_graph.ravel(), len_graph.ravel())[0,1]
+rho, _ = spearmanr(geom_graph.ravel(), len_graph.ravel())
+
+
+x_pos_text = 0.15
+y_pos_text = 110
+offset_text = 7
+
+pl.text(x_pos_text, y_pos_text, 
+	    r'''Pearson's $R = {:.2f}$'''.format(R), fontsize=16)
+
+pl.text(x_pos_text, y_pos_text+offset_text, 
+	    r'''Spearman's $\rho = {:.2f}$'''.format(rho), fontsize=16)
+
+
+
+pl.savefig('/data/hu_paquette/work/tractoGRAPHy/ismrm2022/images/'+'geom_len_correlation_density.png',
+			dpi=300,
+			pad_inches=0.25,
+			transparent=True,
+			bbox_inches='tight')
+
+pl.close()
+
+# pl.show()
 
 
